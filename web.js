9593208f08d57cffcf6b784e42180c4cc7d28bc0
Robin Spears <sweetvagabond@gmail.com>
2013-10-20 21:24:31 -0500

    Add a static directory.

--- web.js
+++ web.js
@@ -3,11 +3,7 @@ var app = express();
 var visitorCount = 0;
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-    visitorCount += 1;
-    response.send('Hello! You are visitor #' + visitorCount + '.');
-});
-
+app.use(express.static(__dirname + '/static'));
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log('Listening on ' + port);
