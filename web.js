76a57514d776a8a80757fa34f4247c9e43fb0a17
OpenShift guest <sweetvagabond@gmail.com>
2013-10-20 21:03:46 -0400

    Consistency.

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 app.use(express.logger());
 
@@ -8,5 +8,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+    console.log('Listening on ' + port);
 });
